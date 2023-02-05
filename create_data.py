4d069c57ce772129bc18bef7ce47779a6f3b2a4d
AuthorCSE5 <124360048+AuthorCSE5@users.noreply.github.com>
2023-02-05 13:55:59 +0530

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -76,6 +76,6 @@ def create_data(train_set1, train_set2, num_items, flag, t):
         data[i,3] = np.sum(final_data_sign == -1)
 
         
-    np.savetxt("BTLData_"+flag+"_"+str(t)+".txt", data)
+    np.savetxt("BTLData_"+flag+"_"+str(t)+".txt", data)      ##Change the dataset name for other 2 datsets (SF/DF/BTL)
         
     return data, num_pairs
