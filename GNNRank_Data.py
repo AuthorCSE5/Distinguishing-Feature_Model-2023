0b231f7fc5ad0c4e1dba82f5c693759d22084cd6
AuthorCSE5 <124360048+AuthorCSE5@users.noreply.github.com>
2023-02-09 12:29:48 +0530

    Update GNNRank_Data.py

--- GNNRank_Data.py
+++ GNNRank_Data.py
@@ -7,7 +7,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 import Real_Data
-import create_data2
 import create_data3
 
 
@@ -37,30 +36,18 @@ K = 11'''
 
 num_items = 2287
 num_games = 28582
-K = 12
+K = 12     #K = int(math.log2(num_items)) + 1 #ceiling
 
 print("num_items = ", num_items)
 
-Dota = Dota[num_items+1:,0:]
+Dota = Dota[num_items+1:,0:]  ##for HotS and WoL
+#Dota = Dota[num_items+1:,1:]  ##for movielens
 Dota = Dota.astype('int')
-#Dota = Dota[1:,:]
+#Dota = Dota[1:,:]  ##for jester and Dota
 
 print("Dota = ", Dota, "size = ", np.shape(Dota)[0])
-#print(int(Dota[0][2]), int(Dota[0][3]))
-#print(int(Dota[0][1]), int(Dota[0][2]))
-print(Dota)
 
-'''num_items = 757 #Dota[0,0]
-#num_items = 100
-print("num_items = ", num_items)
-data_size = Dota[0,0]
-print("data_size = ", data_size)
-
-Dota = Dota[1:,:]
-print("Dota = ", Dota, "size = ", np.shape(Dota)[0])'''
-
-seed_range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for seed in seed_range[0:1]:
+for seed in range(10):
  
     #train_data1, train_data2, train_val_data1, train_val_data2, val_data1, val_data2, test_data1, test_data2, prob_data, prob_val = Real_Data.Real_Dataset(Dota, int(num_items), seed)
 
@@ -68,21 +55,17 @@ for seed in seed_range[0:1]:
     #data2 = np.append(train_data2,test_data2,axis = 0)
     #data_final = create_data.create_data(data1, data2, num_items, seed)
 
-    #train_data, test_data = train_test_split(Dota, test_size=0.30, random_state=seed)
+    train_data, test_data = train_test_split(Dota, test_size=0.30, random_state=seed)
+    train_val_data, val_data = train_test_split(train_data, test_size=0.20, random_state=seed)
 
-
-    #np.savetxt("GNNRank_Datasets_finer/HotS_Gnnrank_train"+str(seed)+"_preprocessing.txt", train_data)
-    #np.savetxt("GNNRank_Datasets_finer/HotS_Gnnrank_test"+str(seed)+"_preprocessing.txt", test_data)
+    np.savetxt("GNNRank_Datasets_finer/HotS_Gnnrank_train"+str(seed)+"_preprocessing.txt", train_data)
+    np.savetxt("GNNRank_Datasets_finer/HotS_Gnnrank_test"+str(seed)+"_preprocessing.txt", test_data)
     
-    #train_val_data, val_data = train_test_split(train_data, test_size=0.20, random_state=seed)
-
+    
     #train_data = np.loadtxt("GNNRank_Datasets_finer/DOTA_Gnnrank_train"+str(seed)+"_preprocessing.txt", dtype = 'float')
-    #data_final1, train_pairs = create_data3.create_data(train_data, num_items, 'train', seed)
-    test_data = np.loadtxt("GNNRank_Datasets_finer/HotS_Gnnrank_test"+str(seed)+"_preprocessing.txt", dtype = 'float')
+    data_final1, train_pairs = create_data3.create_data(train_data, num_items, 'train', seed)
+    #test_data = np.loadtxt("GNNRank_Datasets_finer/HotS_Gnnrank_test"+str(seed)+"_preprocessing.txt", dtype = 'float')
     data_final2, test_pairs = create_data3.create_data(test_data, num_items, 'test', seed)
 
-    #data_final1 = create_data2.create_data(train_data1, train_data2, num_items, 'train', seed)
-    #data_final2 = create_data2.create_data(test_data1, test_data2, num_items, 'test', seed)
-
-
+    
 
